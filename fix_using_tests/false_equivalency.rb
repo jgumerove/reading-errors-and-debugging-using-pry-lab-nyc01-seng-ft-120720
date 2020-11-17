ba1d5646842480de249529eff2958d45f2eb8faa
Josh Gumerove <josh.gumerove@gmail.com>
2020-11-17 04:47:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,12 +11,11 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      Puts "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      Puts "HAM HAM HAM IN MY TUMMY"
   end
-  binding.pry
   puts "hello"
 end
 
